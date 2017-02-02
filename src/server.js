313d3f24e8be948d0b090c829e6af132ea3c3449
MROwens <Matt_Owens@Matts-MacBook-Pro-2.local>
2017-02-01 23:59:12 -0500

    Basic File Stucture & Install Node

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
 
+var app = express();
+
+var port = 3000;
+
+app.get('/status', (req,res) => {
+  res.json({"status": "We Good"});
+});
+
+
+app.listen(port, () => {
+  console.log("Live");
+});
