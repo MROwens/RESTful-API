91746795ae8f21365aee5e9b65b1e813af5e0d68
Matt Owens <Wulfy65@gmail.com>
2017-02-02 21:31:52 -0500

    URL Shortener Entry Point

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,26 @@
 module.exports = (express) => {
+
+  var urlGenerator = require('../src/modules/url-shortener.js');
+
   // Instantiate express Router method
   var router = express.Router();
   // Check if server is connection is good
   router.get('/status', (req,res) => {
     res.json({"status": "Connected"});
   });
-
+  // Entry point for API
   router.post('/url', (req,res) => {
+    // Parse the body for origin link
+    var url = req.body.origin;
 
-
+    var generateShortUrl = urlGenerator.urlGen(6);
+    // Validation
+    if(url === ''){
+      res.json({Origin: "No URL Provided", New: "No URL Provided"});
+    }else {
+      res.json({Origin: url, New: generateShortUrl});
+    }
   });
-
 
   return router;
 }
--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ var app = express();
 
 var port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
 // Linking to routes
 app.use("/api/v1", require('../routes/api.js')(express));
 
