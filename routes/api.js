db01a38d3eeab2804b510fa4d810794360b22acf
Matt Owens <Wulfy65@gmail.com>
2017-02-02 20:32:14 -0500

    Url Shortener Logic & Comments

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
 module.exports = (express) => {
-
+  // Instantiate express Router method
   var router = express.Router();
-
+  // Check if server is connection is good
   router.get('/status', (req,res) => {
-    res.json({"status": "We Good"});
+    res.json({"status": "Connected"});
+  });
+
+  router.post('/url', (req,res) => {
+
+
   });
 
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
+// Packages needed
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
 var port = 3000;
 
+// Linking to routes
 app.use("/api/v1", require('../routes/api.js')(express));
 
+// Listen on specified port
 app.listen(port, () => {
   console.log("Live");
 });
